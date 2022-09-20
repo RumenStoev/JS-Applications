08790bc76cdc5269dd88f77ca8da168dd9baf878
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-09-20 15:45:58 +0300

    Update app.js

--- Asynchronous Programming/03.Gighub-Commits/app.js
+++ Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,13 +1,13 @@
-async function loadCommits() {
+  async function loadCommits() {
 	let inputName = document.getElementById("username");
 	let inputRepo = document.getElementById("repo");
 	let list = document.getElementById("commits");
 	let request =  await fetch(`https://api.github.com/repos/${inputName.value}/${inputRepo.value}/commits`)
 
-     list.innerHTML = ""
+        list.innerHTML = ""
 
-    if(request.ok) {
-       let data = await request.json();
+       if(request.ok) {
+          let data = await request.json();
 	   data.forEach(({commit}) => {
 		 insertNameAndMessage(commit.author.name,commit.message)
 	   })
@@ -21,7 +21,7 @@ async function loadCommits() {
 		let li = document.createElement("li");
 	        li.innerHTML = `${authorName}:${message}`;
 		
-			list.appendChild(li);
+		list.appendChild(li);
 	}
 }
 
