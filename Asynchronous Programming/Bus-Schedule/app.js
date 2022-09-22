1a51f623b34d6d841e02a054c2602f4fdd3abef1
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-09-22 18:25:04 +0300

    Create app.js

---
+++ Asynchronous Programming/Bus-Schedule/app.js
@@ -0,0 +1,45 @@
+ function solve() {
+    const infoBus = document.querySelector("#info > span");
+    const buttonDepart = document.getElementById("depart");
+    const buttonArrive = document.getElementById("arrive");
+    
+    let nextStop = "depot"
+    let dataName = ""
+
+    async function depart() {
+       takeInfoBuse(nextStop);
+       templateInfo(`Next stop ${dataName}`,true,false)
+    }
+
+    async function arrive() {
+          takeInfoBuse(nextStop)
+          templateInfo(`Arriving at ${dataName}`,false,true)
+
+    }
+
+    async function takeInfoBuse(name) {
+       try { 
+            let response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${name}`)
+            let data = await response.json();
+                dataName = data.name;
+            
+       } catch(e) {
+           infoBus.innerHTML = "Error"
+           buttonDepart.disabled = true;
+           buttonArrive.disabled = false;
+       }
+    }
+
+    function templateInfo(text,disabledDepart,disabledArrive) {
+        infoBus.innerHTML = text;
+        buttonDepart.disabled = disabledDepart
+        buttonArrive.disabled = disabledArrive
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
