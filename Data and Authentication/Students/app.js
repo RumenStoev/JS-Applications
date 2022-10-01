7ccf3c94cdf6b5551bd58a46b7e2be0cc2c39f81
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-10-01 11:42:09 +0300

    Update app.js

--- Data and Authentication/Students/app.js
+++ Data and Authentication/Students/app.js
@@ -1,61 +1,60 @@
-
 let urlStudents = "http://localhost:3030/jsonstore/collections/students"
-
 
 let getStudents = async(url) => {
 	try {
-	  return await fetch(url)
+	     return await fetch(url)
 	                  .then(data => {
-		        return data.json() })
-					        
-	} catch(err) {
+				return data.json()
+			 })
+	   }catch(err) {
 		throw new Error(err)
 	}
 }
 
 let sendStudent = async(data) => {
 	  return await fetch("http://localhost:3030/jsonstore/collections/students",{
-	                        	method: "post",
-	                        	headers: {"Content-Type" : "application/json"},
-		                        body: JSON.stringify(data)
-	                   })
+		            method: "post",
+		            headers: {"Content-Type" : "application/json"},
+		            body: JSON.stringify(data)
+	  })
 }
 
-let displayStudentsData = () => {
+let displayStudentsData = async () => {
 	let submitButton = document.getElementById("submit");
-	let data = document.querySelectorAll(".inputs > input")
+	let data = document.getElementById("form")
   
 	 submitButton.addEventListener("click",async(e) => {
           e.preventDefault()
-	    let [name,lastName,number,grade] = data;
-
-		 await sendStudent({
-	      		      firstName:name.value,
-			      lastName:lastName.value,
-			      number:number.value,
-			      grade:grade.value
-		 }).then(data => {
-			
-        return getStudents(urlStudents)
+	    
+	     let dataStudent = new FormData(data);
+		
+		 dataStudent = [...dataStudent.entries()]
+		
+                 dataStudent = Object.fromEntries(dataStudent)
+		
+	       await sendStudent(dataStudent).then(data => {
+		
+           return getStudents(urlStudents)
 		 
-      }).then(data => {
+          }).then(data => {
 			
-        displayTable(data)		
+              displayTable(data)
 		 })	 
 	 })
 }
 
 let displayTable = (data) => {
-  let tbody = document.querySelector("tbody");
-  let td = (text) => `<td>${text}</td>`
+    let tbody = document.querySelector("tbody");
+    let td = (text) => `<td>${text}</td>`
        
 	tbody.innerHTML = ""
 
 	Object.values(data).forEach(item => {
 	   	let tr = document.createElement("tr")
-		let dataStudent = [item.firstName,item.lastName,item.number,item.grade]
-		  dataStudent.forEach(x => tr.innerHTML+=td(x))
-		  tbody.appendChild(tr)
+		let dataStudent = [item.firstName,item.lastName,item.facultyNumber,item.grade]
+		 
+		dataStudent.forEach(x => tr.innerHTML+=td(x))
+		tbody.appendChild(tr)
 	  })
 }
 
