2f1fd75c7d0e32b22d7c08a2c41de5ae1ced5be2
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-10-07 17:19:57 +0300

    Create app.js

---
+++ Data and Authentication/BookLibrary/src/app.js
@@ -0,0 +1,16 @@
+
+import { loadAllBooks, submitButton } from "./buttons.js"
+
+async function attachEvents() {
+
+       return await submitButton()
+}
+
+async function loadBooks() {
+    return await loadAllBooks()
+}
+
+
+
+loadAllBooks() 
+attachEvents()
