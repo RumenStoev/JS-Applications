30c7efb0b48c022a8884f872faabf0aa9923e4f9
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-04 20:22:46 +0200

    Update filters.js

--- SPA/SoftForum/src/filters.js
+++ SPA/SoftForum/src/filters.js
@@ -2,5 +2,12 @@ let takeDataPostForm = (form) =>  Object.fromEntries([...new FormData(form).entr
 
 let clearInputFields = (inputs) => inputs.values(input => input.value = "") 
 
+let getAllTopics = async(topics) => await Object.values(topics)
 
-export { takeDataPostForm, clearInputFields }
+let renderAllTopics = async(topics,fn) => await topics.forEach(x => fn(x))
+
+let takeLastTopic = async(topic) => (await getAllTopics(topic)).pop()
+
+export { takePostFormData, clearInputFields, getAllTopics, renderAllTopics, takeLastTopic}
+
+
