f018c3e8b7feda3c6c3a7a0b802b284a73702a24
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-06 18:52:21 +0200

    Create PostManager.js

---
+++ SPA/SoftForum/src/PostPage/PostManager.js
@@ -0,0 +1,9 @@
+import { PostForm } from "./PostEngine/postForm.js";
+import { RenderTopics} from "./PostEngine/renderTopics.js";
+
+
+//Every Topic has reference to CommentPage!
+
+
+await RenderTopics()
+await PostForm()
