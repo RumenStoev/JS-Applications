e8c1569a7b47d21f6cac40ef4bceca96d229b36a
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-14 20:51:00 +0200

    Create postForm.js

---
+++ SPA/Forum/src/PostPage/PostEngine/postForm.js
@@ -0,0 +1,45 @@
+import { getDataForm, clearInputFields } from "./postHelpers/postFilters.js"
+import { sendPost } from "./postHelpers/postRequests.js"
+import { topicTemplate } from "./postHelpers/postTemplates.js";
+
+
+
+let postForm = document.getElementById("postForm")
+let fieldsForm = document.querySelectorAll("#topicName, #username, #postText")
+
+
+async function PostForm() {
+ 
+    document.addEventListener("click",async(e) => {
+        e.preventDefault()
+
+        let currButton = e.target.className;
+   
+        if(typeof casePostPage[currButton] === "function" ) { 
+            casePostPage[currButton]()
+        }  
+    })
+}
+
+let casePostPage = {
+    "cancel":  await btnCancel,
+    "public":  await btnPost
+}
+
+async function btnPost() {
+    return await getDataForm(postForm)
+                    .then(topicTemplate)
+                    .then(sendPost)
+                    .then(clearInputFields(fieldsForm))
+    
+}
+
+async function btnCancel() {
+      
+    return await clearInputFields(fieldsForm)
+}
+
+
+
+
+export { PostForm }
