17f588311e73a78fc09ca1feda102cc12fd84e47
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-14 20:42:22 +0200

    Delete SPA/SoftForum/src directory

--- SPA/SoftForum/src/CommentsPage/CommentsEngine/filters.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-let takeSelectedTopic = async(topics,target) => await Object.values(topics).filter(topic => topic.topicName === target ).pop()
-//let renderTopic = async(topic) =>  
-
-
-export { takeSelectedTopic }
