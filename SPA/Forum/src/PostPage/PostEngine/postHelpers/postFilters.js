bae23b666c8cc9bc961350b4d3f11d5d74991ee8
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-14 20:52:36 +0200

    Create postFilters.js

---
+++ SPA/Forum/src/PostPage/PostEngine/postHelpers/postFilters.js
@@ -0,0 +1,18 @@
+///////////////////FORM Filters///////////////////////////////////
+let getDataForm = async (form) =>  {
+    let postForm = Object.fromEntries([...new FormData(form).entries()])
+        postForm["Date"] = new Date().toLocaleString();
+        
+    
+     return await postForm   
+}
+
+
+let clearInputFields = (inputs) => inputs.forEach(input =>  input.value = "")
+
+let renderAllTopics = async(topics,topicTemplate) => await Object.values(topics).forEach(topic => topicTemplate(topic))
+
+/////////////////////////////////////////////////////////////////////
+
+
+export { getDataForm, clearInputFields, renderAllTopics, isValidJSON }
