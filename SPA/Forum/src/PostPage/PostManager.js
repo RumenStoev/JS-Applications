317c82a5db99b9fc2c0b6e33ccf37c9809988023
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-15 21:41:05 +0200

    Update PostManager.js

--- SPA/Forum/src/PostPage/PostManager.js
+++ SPA/Forum/src/PostPage/PostManager.js
@@ -1,14 +1,21 @@
 import { PostForm } from "./PostEngine/postForm.js";
 import { RenderTopics } from "./PostEngine/renderAllTopics.js";
 
+class PostPage {
 
+    constructor(clickedTarget) {
+        this.clickedTarget = clickedTarget
+    }
 
+    async postForm() {
+        return await PostForm(this.clickedTarget)
+    }
 
-async function PostPage() {
-    await RenderTopics()
-    await PostForm()
+    async renderAllTopics() {
+        return await RenderTopics()
+    }
+
 }
-
 
 
 export { PostPage }
