8af907be3cfa5a20400b54955d03ae2176b83861
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-11-16 19:38:21 +0200

    Update forumManager.js

--- SPA/Forum/src/forumManager.js
+++ SPA/Forum/src/forumManager.js
@@ -1,18 +1,40 @@
 import { PostPage } from "./PostPage/PostManager.js"
+import { renderTopic, postDataFromForm,renderAllComments } from "./CommentsPage/comments.js"
 
- new PostPage().renderAllTopics()
 
-  document.addEventListener("click", async (e) => {
-     e.preventDefault()
 
-    let currentTarget = e.target.className;
+async function Forum() {
 
-    if (currentTarget === "public" || currentTarget === "cancel") {
-        return await new PostPage(currentTarget).postForm()
-    }
-      
-})
+    document.addEventListener("click", async (e) => {
+        e.preventDefault()
 
+        let currentButton = e.target.className
+        let clickedTopic = e.target.tagName
+        
+        if (currentButton === "public" || currentButton === "cancel") {
+            return await new PostPage(currentButton).postForm()
+        }
+
+        if(clickedTopic === "H2") {
+             let nameTopic = e.target.textContent
+
+             await renderTopic(nameTopic)
+             await renderAllComments()
+        }
+
+        if(e.target.tagName === "BUTTON" && e.target.textContent === "Post") {
+             return await postDataFromForm(e.target);
+        }
+
+    })
+
+    new PostPage().renderAllTopics()
+  
+
+}
+
+
+await Forum()
 
 
 
