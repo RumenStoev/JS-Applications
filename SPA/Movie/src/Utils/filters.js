7a747ec9882c5972ea386f460e39f4df7d4bcbfe
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-12-13 20:40:15 +0200

    Create filters.js

---
+++ SPA/Movie/src/Utils/filters.js
@@ -0,0 +1,34 @@
+
+let getDataForm = (formTag) => Object.fromEntries([...new FormData(formTag).entries()])
+
+let isValidForm = async (formData,requirements,errorMsg) => {
+
+    let dataForm = getDataForm(formData)    
+    
+    let checkFormData = Object.values(dataForm);
+
+     if(requirements !== undefined && typeof dataForm === "object") {   
+   
+      Object.values(requirements).forEach((checkForm,i) => {
+        
+            if(!checkForm(checkFormData[i],checkFormData[i - 1])) {
+                throwError(errorMsg)      
+            }
+            
+    });
+ }
+    return dataForm
+   
+ }
+
+
+ let throwError = (msg) => { //Two storages one for Developer one for a client!
+      throw new Error(msg)
+ } 
+
+ let redirectToHomePage = (statusRequest) => {
+    if(statusRequest === "OK") window.location.replace("http://localhost:4000/")
+ }
+
+
+export { getDataForm,isValidForm, redirectToHomePage }
