78c61b04a6730b05255ab32ea362328f0a8707ad
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-12-14 18:33:17 +0200

    Update MovieController.js

--- SPA/Movie/src/MovieController.js
+++ SPA/Movie/src/MovieController.js
@@ -1,5 +1,6 @@
 import { registerUserProcess } from "./Components/Nav-Bar/RegisterPage/registerDataProcess.js"
 import { loginUserProcess } from "./Components/Nav-Bar/LoginPage/loginPage.js"
+import { redirectToHomePage } from "./Utils/filters.js"
 import { router } from "./Router.js"
 
 
@@ -9,10 +10,14 @@ document.addEventListener("DOMContentLoaded",(e) => {
      e.preventDefault()
        
      let currentForm = e.target.parentElement.id
-      
+
      if(formsById.hasOwnProperty(currentForm)) {
             formsById[currentForm]()
       }
+
+     if(anchorLinks.hasOwnProperty(e.target.id)) {
+      anchorLinks[e.target.id]()
+     } 
 
   })
 
@@ -22,13 +27,18 @@ document.addEventListener("DOMContentLoaded",(e) => {
 
 let formsById = { 
     "register-form":async() => await registerUserProcess(),
-    "form-login": async () => await loginUserProcess(),
+    "login-form": async () => await loginUserProcess(),
     "edit-movie":() => {},
     "add-movie":() => {},
-    "register":(e,path) => router(),
-    "login":() => console.log("login"),
+    "register":() => router(),
+    "login":() => router() ,
     "logout":() => console.log("logout"),
-    "delete":() => console.log("delete")   
+    "delete":() => console.log("delete"),
+    
+}
+ 
+let anchorLinks = {
+  "header-nav-bar": redirectToHomePage
 }
 
 
