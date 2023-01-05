e5e72e84a780f417c72d3ad6333a3275575f79e9
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-01-05 19:49:54 +0200

    Update homeView.js

--- SPA/Movie/src/Components/HomePage/homeView.js
+++ SPA/Movie/src/Components/HomePage/homeView.js
@@ -1,31 +1,53 @@
+import { checkLoggedEmail, isLogged } from "../../Utils/filters.js"
+import { homePageDisplayProcess } from "./homePage.js"
+
 let homeTemplate = () => {
-    
-    return `
-    <section id="home-page" class="view-section">
-    <div
+  homePageDisplayProcess()
+  return `
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand text-light" href="/" id="header-nav-bar">Movies</a>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item user">
+           
+            <a class="nav-link" id="welcome-msg">Welcome, ${checkLoggedEmail()}</a>
+          </li>
+          <li class="nav-item user" id="logout">
+            <a class="nav-link" href="/logout">Logout</a>
+          </li>
+          <li class="nav-item guest" id="login">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+          <li class="nav-item guest" id="register">
+            <a class="nav-link" href="/register">Register</a>
+          </li>
+        </ul>
+      </nav>
+      <section id="home-page" class="view-section">
+      <div
       class="jumbotron jumbotron-fluid text-light"
       style="background-color: #343a40"
-    >
+      >
       <img
-        src="https://slicksmovieblog.files.wordpress.com/2014/08/cropped-movie-banner-e1408372575210.jpg"
-        class="img-fluid"
-        alt="Responsive image"
-        style="width: 150%; height: 200px"
+          src="https://slicksmovieblog.files.wordpress.com/2014/08/cropped-movie-banner-e1408372575210.jpg"
+          class="img-fluid"
+          alt="Responsive image"
+          style="width: 150%; height: 200px"
       />
       <h1 class="display-4">Movies</h1>
       <p class="lead">
-        Unlimited movies, TV shows, and more. Watch anywhere. Cancel
-        anytime.
+          Unlimited movies, TV shows, and more. Watch anywhere. Cancel
+          anytime.
       </p>
-    </div>
+      </div>
 
-    <h1 class="text-center">Movies</h1>
+      <h1 class="text-center">Movies</h1>
 
-    <section id="add-movie-button" class="user">
-      <a href="#" class="btn btn-warning">Add Movie</a>
-    </section>
-  
-    <section id="movie">
+      <section id="add-movie-button" class="user" style="display:${isLogged()}">
+        <a href="#" class="btn btn-warning">Add Movie</a>
+      </section>
+    
+      <section id="movie">
       <div class="mt-3">
         <div class="row d-flex d-wrap">
           <ul
@@ -37,8 +59,8 @@ let homeTemplate = () => {
         </div>
       </div>
     </section>
-  </section>
-    `
+    </section>
+        `
 }
 
 
