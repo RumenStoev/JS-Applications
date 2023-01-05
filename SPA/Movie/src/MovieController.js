60560930e35e0a0dc358cbfb554ee490b8b20c16
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-01-05 19:37:51 +0200

    Update MovieController.js

--- SPA/Movie/src/MovieController.js
+++ SPA/Movie/src/MovieController.js
@@ -1,23 +1,29 @@
-import { registerUserProcess } from "./Components/Nav-Bar/RegisterPage/registerDataProcess.js"
-import { loginUserProcess } from "./Components/Nav-Bar/LoginPage/loginPage.js"
-import { redirectToHomePage } from "./Utils/filters.js"
+import { registerUserProcess } from "./Components/Nav-Bar/RegisterPage/registrationProcess.js"
+import { loginUserProcess } from "./Components/Nav-Bar/LoginPage/loginProcess.js"
+import { addMovieTemplate } from "./Components/HomePage/AddMoviePage/addMovieView.js"
+import { addMovieProcess } from "./Components/HomePage/AddMoviePage/addMovieProcess.js"
 import { router } from "./Router.js"
+import { logout } from "./Utils/requests.js"
+import { detailsTemplate } from "./Components/HomePage/DetailsMoviePage/detailsView.js"
+import { editMovieProcess } from "./Components/HomePage/EditMoviePage/editMovieProcess.js"
+import { clickLikeBtn, deleteMovieProcess, displayDetailsMovie } from "./Components/HomePage/DetailsMoviePage/detailsMovie.js"
+import { submitEditForm } from "./Components/HomePage/EditMoviePage/editMovieProcess.js"
+import { displayPage } from "./Utils/filters.js"
 
+document.addEventListener("DOMContentLoaded", () => {
 
-document.addEventListener("DOMContentLoaded",(e) => {
+  document.addEventListener("click", (e) => {
+    e.preventDefault()
 
-  document.addEventListener("click",(e) => {
-     e.preventDefault()
-       
-     let currentForm = e.target.parentElement.id
+    let currentForm = e.target.parentElement.id
 
-     if(formsById.hasOwnProperty(currentForm)) {
-            formsById[currentForm]()
-      }
+    if (formsById.hasOwnProperty(currentForm)) {
+      formsById[currentForm](e.target)
+    }
 
-     if(anchorLinks.hasOwnProperty(e.target.id)) {
+    if (anchorLinks.hasOwnProperty(e.target.id)) {
       anchorLinks[e.target.id]()
-     } 
+    }
 
   })
 
@@ -25,21 +31,26 @@ document.addEventListener("DOMContentLoaded",(e) => {
 
 })
 
-let formsById = { 
-    "register-form":async() => await registerUserProcess(),
-    "login-form": async () => await loginUserProcess(),
-    "edit-movie":() => {},
-    "add-movie":() => {},
-    "register":() => router(),
-    "login":() => router() ,
-    "logout":() => console.log("logout"),
-    "delete":() => console.log("delete"),
-    
+let formsById = {
+  "register-form": () => registerUserProcess(),
+  "login-form": () => loginUserProcess(),
+  "add-movie-button": () => displayPage(addMovieTemplate()),
+  "add-movie-form": () => addMovieProcess(),
+  "register": () => router(),
+  "login": () => router(),
+  "logout": () => logout(),
+  "btnDetailsMovie": (e) => displayDetailsMovie(e),
+  "editForm": () => submitEditForm()
 }
- 
+
 let anchorLinks = {
-  "header-nav-bar": redirectToHomePage
+  "header-nav-bar": router,
+  "movieDetails": () => detailsTemplate(),
+  "edit": () => editMovieProcess(),
+  "likeBtn": () => clickLikeBtn(),
+  "deleteBtn": () => deleteMovieProcess()
 }
+
 
 
 
