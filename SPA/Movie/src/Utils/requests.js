cbf1f70eb1769caea1068e91f867a8253c30f928
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-01-05 19:38:30 +0200

    Update requests.js

--- SPA/Movie/src/Utils/requests.js
+++ SPA/Movie/src/Utils/requests.js
@@ -1,4 +1,6 @@
 
+import { redirectToLoginPage } from "./filters.js"
+
 let serverURL = `http://localhost:3030/`
 let moviePath = `data/movies`
 let likesPath = `data/likes`
@@ -6,160 +8,201 @@ let usersRegister = `users/register`
 let usersLogin = `users/login`
 
 
+////AccessToken for the Server////
+let accessToken = JSON.parse(sessionStorage.getItem("loginData")).accessToken
+/////////////////////////////////
+
 async function getMovies() {
     return await fetch(serverURL + moviePath)
-            .then(isValidJSON)
-            .catch(catchError)
+        .then(isValidJSON)
+        .catch(catchError)
 }
 
 
 async function createMovie(data) {
-    return await fetch(serverURL + moviePath,{
+    return await fetch(serverURL + moviePath, {
         method: "post",
         headers: {
-            "Content-Type":"application/json",
-            "X-Authorization":sessionStorage.getItem("token")
-     },
-        body: JSON.stringify(data) 
+            "Content-Type": "application/json",
+            'X-Authorization': accessToken
+
+        },
+        body: JSON.stringify(data)
     })
-    .catch(catchError)
+        .then(x => x.status)
+        .catch(catchError)
 }
 
 
-async function updateMovie(id) {
-     return await fetch(serverURL + moviePath + `/${id}`,{
-        method: "put",
-        headers: {
-            "Content-Type":"application/json",
-            "X-Authorization":sessionStorage.getItem("token")
-        },
-        body: JSON.stringify(data)
-     })
-     .catch(catchError)
+async function updateMovie(data, id) {
+    let idMovie = JSON.parse(sessionStorage.getItem("dataMovie"))._id
+
+    try {
+
+        let request = await fetch(serverURL + moviePath + `/${idMovie}`, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+                "X-Authorization": accessToken
+            },
+            body: JSON.stringify(data)
+        })
+
+        return request.status
+
+    } catch (e) {
+
+        catchError(e)
+    }
+
 }
 
 
 async function deleteMovie(id) {
-    return await fetch(serverURL + moviePath + `/${id}`,{
+    return await fetch(serverURL + moviePath + `/${id}`, {
         method: "delete",
         headers: {
-            "Content-Type":"application/json",
-            "X-Authorization":sessionStorage.getItem("token")
+            "Content-Type": "application/json",
+            "X-Authorization": accessToken
         }
     })
-    .catch(catchError)
-        
+        .then(request => request.status)
+        .catch(catchError)
+
 }
 
 
 async function getMovieLikes(movieId) {
-    let likesPath = `/data/likes?where=movieId%3D%22${movieId}%22&distinct=_ownerId&count`
-    
-    return await fetch(serverURL + likesPath)
-               .then(isValidJSON)
-               .catch(catchError)
+    let allLikesPath = `data/likes?where=movieId%3D%22${movieId}%22&distinct=_ownerId&count`
+    try {
+        let response = await fetch(serverURL + allLikesPath)
+
+        return await response.json()
+    } catch (e) {
+        catchError(e)
+    }
+
 }
 
 
-async function getLikeSpecificUser(movieId,userId) {
-    let likeSpecificUser = `/data/likes?where=movieId%3D%22${movieId}%22%20and%20_ownerId%3D%22${userId}%22 `
-    
-    return await fetch(serverURL + likeSpecificUser)
-              .then(isValidJSON)
-              .catch(catchError)
+async function getLikeSpecificUser(movieId, userId) {
+    try {
+        const response = await fetch(`${serverURL}data/likes?where=movieId%3D%22${movieId}%22%20and%20_ownerId%3D%22${userId}%22`)
+        return await response.json()
+    } catch (e) {
+        catchError(e)
+    }
+
 }
 
-async function addLike(data) {
-   
-    return await fetch(serverURL + likesPath,{
+async function addLike(id) {
+
+    return await fetch(serverURL + likesPath, {
         method: "post",
         headers: {
-            "Content-Type":"application/json",
-            "X-Authorization":sessionStorage.getItem("token")
+            "Content-Type": "application/json",
+            "X-Authorization": accessToken
         },
-        body:JSON.stringify(data)
+        body: JSON.stringify({ movieId: id })
     })
-    .catch(catchError)
+        .then(x => x.json())
+        .catch(catchError)
 }
 
 
 async function revokeLike(likeId) {
-   return await fetch(serverURL + likesPath + `/${likeId}`,{
-    method: "delete",
-    headers: {
-        "Content-Type":"application/json",
-        "X-Authorization":sessionStorage.getItem("token")
-    }
-   })
-   .catch(catchError)
-} 
-
+    return await fetch(serverURL + likesPath + `/${likeId}`, {
+        method: "delete",
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization": accessToken
+        }
+    })
+        .then(request => request.json())
+        .catch(catchError)
+}
 
 
 async function registerUser(data) {
-   try {
-    let request =  await fetch(serverURL + usersRegister,{
-        method: "post",
-        headers:{"Content-Type":"application/json",
-        
-    },
-        body: JSON.stringify(data)
-    })
-    
-    if(request.ok) {
-        const dataRequest = await request.json();
+    try {
+        let request = await fetch(serverURL + usersRegister, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
 
-        sessionStorage.setItem("data",JSON.stringify(dataRequest))
-		return request.status
+            },
+            body: JSON.stringify(data)
+        })
+        if (request.ok) {
+            const dataRequest = await request.json();
+
+            sessionStorage.setItem("data", JSON.stringify(dataRequest))
+            return request.status
+        }
+    } catch (e) {
+        catchError(e)
     }
-  } catch(e) {
-    catchError(e)
- }
-            
-}
 
+
+}
 async function loginUser(data) {
-   
+
     try {
 
         let request = await fetch(serverURL + usersLogin, {
-        method: "post",
-        headers: {"Content-Type":"application/json"},
-        body: JSON.stringify(data)
-    })
-    
-     if(request.ok) {
-        sessionStorage.setItem("loginData",JSON.stringify(request.json()))
-        return request.status
-     }else {
-        return request.status
-     }
-    
-    
- } catch(e) {
-    
-   catchError(e)
- }
-   
-                
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        })
+
+        if (request.ok) {
+            sessionStorage.setItem("loginData", JSON.stringify(await request.json()))
+            return request.status
+
+        } else {
+            return request.status
+        }
+
+
+    } catch (e) {
+
+        catchError(e)
+    }
+
+
+}
+
+async function logout() {
+    try {
+        let request = await fetch("http://localhost:3030/users/logout", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: sessionStorage.getItem("loginData")
+
+        })
+        if (request.ok) {
+
+            sessionStorage.removeItem("loginData");
+            redirectToLoginPage()
+        }
+
+    } catch (e) {
+        catchError(e)
+    }
+}
+
+
+let isValidJSON = async (data) => {
+    if (!data.ok) throw new Error(data.statusText)
+    return data
 }
 
 
 
-let isValidJSON = async(data) => {
-    if(!data.ok) throw new Error(data.statusText)
-    return data;
-}
-let setSessionStorage = async(data) =>  sessionStorage.setItem('set',JSON.stringify(data))
-
-
-let catchError = async(data) => {
-     return Promise.reject(data)
-}
-
-
-let successData = async(data) => { //SessionStorage ?????
-    if(data.ok) sessionStorage.setItem()
+let catchError = async (data) => {
+    return Promise.reject(data)
 }
 
 
@@ -174,5 +217,6 @@ export {
     addLike,
     revokeLike,
     registerUser,
-    loginUser
+    loginUser,
+    logout
 }
