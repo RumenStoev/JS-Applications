07a92cf3d611cf206cca8e49806cebbf9e9ef5d1
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-01-12 17:46:13 +0200

    Update helpers.js

--- Client Side Rendering/Book-Library/helpers.js
+++ Client Side Rendering/Book-Library/helpers.js
@@ -1,23 +1,43 @@
-import { createTable, tBodyTemplate } from "./templates.js";
+import { createTable, tBodyTemplate, editFormTemplate } from "./templates.js";
 import { containerRequests } from "./requests.js";
 
+
 class HelperFunctionality {
-    constructor() { }
 
-    async form() {
+
+    form() {
         return {
-            "getDataForm": async () => {
-                let data = Object.fromEntries([...new FormData(form).entries()]);
 
-                return data;
+            "getDataForm": function (tagForm) {
+                const dataForm = [...new FormData(tagForm).entries()]
+
+                console.log(dataForm)
+                return (this.checkIsEmptyForm(dataForm)) ? Object.fromEntries(dataForm) : this.throwErrorForm("Has empty field!!!")
+
+
             },
-            "checkIsEmptyForm": async (input) => {
-                if (input === "") throw new Error("The inputs are empty!")
+            "checkIsEmptyForm": function (dataArrayForm) {
+
+                return dataArrayForm.every((input, index, arr) => arr[index][1] !== "")
+            },
+
+            "throwErrorForm": function (msg) {
+
+                throw new Error(`${msg}`)
             }
         }
     }
 
+    getDataRow(target) {
+        console.log("Target is", target)
+        let currentRow = target.parentNode.parentNode;
 
+        let [author, title] = Array.from(currentRow.children)
+        let id = currentRow.id;
+        console.log(author)
+
+        return { author: author.textContent, title: title.textContent, id: id }
+    }
 
     async renderTable() {
         return await containerRequests
@@ -29,8 +49,18 @@ class HelperFunctionality {
 
     async renderRow(data) {
         let rows = ""
-        Object.values(data).forEach(data => rows += tBodyTemplate({ ...data }))
+        
+        Object.entries(data).forEach(([id, { author, title }]) => rows += tBodyTemplate({ author, title, id }))
+
         return rows
+
+
+    }
+
+    displayEdit({ author, title, id }) {
+
+        let formTarget = document.getElementById("add-form");
+        formTarget.innerHTML = editFormTemplate({ author, title, id })
     }
 
     async displayStateDOM(data) {
@@ -43,11 +73,9 @@ class HelperFunctionality {
 
 let containerHelpers = new HelperFunctionality()
 
-
 export {
     containerHelpers
 }
-
 
 
 
