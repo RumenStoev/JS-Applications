ee31a868c4e0c5f2df78270a37f65e9d46adbd72
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-02-12 13:47:10 +0200

    Update App.js

--- Architecture and Testing/SoftTerest/App.js
+++ Architecture and Testing/SoftTerest/App.js
@@ -1,15 +1,18 @@
 import { Router } from "./EngineApp/RouterEngine.js";
+import { ClickedEventByText } from "./EngineApp/CommandByEvent.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
     document.addEventListener("click", (e) => {
         e.preventDefault();
-  
+
+        if (ClickedEventByText()[e.target.textContent]) ClickedEventByText()[e.target.textContent](e.target)
+        
     })
 
     document.addEventListener("submit", (e) => {
         e.preventDefault()
     })
 
-    
+
 })
