f5b476fa31c39b7bfda65b69268b40e3c9cfd9fd
RumenS <49361738+RumenStoev@users.noreply.github.com>
2023-03-04 10:32:27 +0200

    Update App.js

--- Architecture and Testing/SoftTerest/src/App.js
+++ Architecture and Testing/SoftTerest/src/App.js
@@ -1,8 +1,29 @@
-import { Router } from "./EngineApp/RouterEngine.js";
+import { RouterWay } from "./EngineApp/RouterEngine.js";
 import { ClickedEvent, SubmitEvent } from "./EngineApp/CommandByEvent.js";
+import { DashboardPage } from "./BuiltPages/DashboardPage.js";
+import { HomePage } from "./BuiltPages/HomePage.js";
+import { CreateIdeaPage } from "./BuiltPages/CreateIdeaPage.js";
+import { RegistrationPage } from "./BuiltPages/RegistrationPage.js";
+import { LoginPage } from "./BuiltPages/LoginPage.js";
+import { showIdeaDetails } from "./Services/ideaDetailsService.js";
 
 
 document.addEventListener("DOMContentLoaded", async (e) => {
+
+  RouterWay("/", HomePage)
+
+  RouterWay("/logout", HomePage)
+
+  RouterWay("/register", RegistrationPage)
+
+  RouterWay("/login", LoginPage)
+
+  RouterWay("/dashboard", DashboardPage)
+
+  RouterWay('/create', CreateIdeaPage)
+
+  RouterWay(`/details/id`, showIdeaDetails)
+
 
   document.addEventListener("submit", (e) => {
     e.preventDefault()
